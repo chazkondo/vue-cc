5350f84ff0d10c502b34292226dd393c0f398351
Chaz Kondo <chuckeekondo@gmail.com>
2021-10-08 11:46:56 -1000

    implement beforeMount method to alter data

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 const app = Vue.createApp({
     data() {
         return {
-            data: "Business data"
+            data: "init"
         }
-    }
+    },
+    methods: {
+        async getTestimonial() {
+            const res = await fetch('https://testimonialapi.toolcarton.com/api')
+            const results = await res.json()
+
+            this.data = results[0]
+            console.log(this.data, 'hi')
+        }
+    },
+    beforeMount(){
+        this.getTestimonial()
+     },
 })
 
 app.mount('#app')
